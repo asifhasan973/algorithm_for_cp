589fcdcc25cbb9f21eca64671311978319f3b2c5
Asif Hasan <86647865+asifhasan973@users.noreply.github.com>
2024-05-27 01:34:51 +0600

    Add files via upload

---
+++ B_Binary_Colouring.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+ 
+#define int long long
+#define endl '\n'
+ 
+using namespace std;
+ 
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> ans;
+ 
+        while(n!=0){
+            for (int i = 30; i >= 0 ; i--)
+            {
+               int p = 1LL<<i;
+                if(n>=p){
+                    
+                    ans.push_back(i);
+                    n-= p;
+                    break;
+                }
+            }
+            
+        }
+        
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout<<"2^"<<ans[i]<<" ";
+        }
+        cout<<endl;
+        
+    }
+    
+    return 0;
+}
